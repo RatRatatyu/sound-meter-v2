8801c70722a2b8f00f9be8724d7f4a62e86ef21b
Margarita <kkom.margarita@gmail.com>
2026-06-24 01:46:59 +0500

    feat: integrate PermissionProvider and MainScreen in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:noise_meter_v2/core/providers/permission_provider.dart';
+import 'package:noise_meter_v2/presentation/main_screen_feature/main_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => PermissionProvider()..initialCheck())
+      ],
+      child: const MyApp(),
+    ),
+    
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,18 +21,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
