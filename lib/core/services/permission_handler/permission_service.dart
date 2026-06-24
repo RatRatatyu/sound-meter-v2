bd2ac9718fd25294fe97a92f8213a7cb775bb6f4
Margarita <kkom.margarita@gmail.com>
2026-06-25 01:37:43 +0500

    refactor: move PermissionService to services directory

--- lib/core/services/permission_handler/permission_service.dart
+++ lib/core/services/permission_handler/permission_service.dart
@@ -1,35 +1,35 @@
-import 'package:noise_meter_v2/core/permission_handler/permission_types.dart';
+
+import 'package:noise_meter_v2/core/services/permission_handler/permission_types.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 
 class PermissionService {
+
+
   Future<MicrophonePermissionStatus> checkMicrophonePermission() async{
     final status = await Permission.microphone.status;
 
     if (status.isGranted) {
       return MicrophonePermissionStatus.granted;
     }
-
     if (status.isPermanentlyDenied) {
       return MicrophonePermissionStatus.permanentlyDenied;
     }
-
     return MicrophonePermissionStatus.denied;
   }
+
 
   Future<MicrophonePermissionStatus> requestMicrophonePermission() async {
     final status = await Permission.microphone.request();
-
     if (status.isGranted) {
       return MicrophonePermissionStatus.granted;
     }
-
     if (status.isPermanentlyDenied) {
       return MicrophonePermissionStatus.permanentlyDenied;
     }
-
     return MicrophonePermissionStatus.denied;
   }
+
 
   Future<void> openSettings() async{
     await openAppSettings();
