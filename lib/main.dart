a733f869f50dfb31c0e1ac348e201e6e11a6bf60
Margarita <kkom.margarita@gmail.com>
2026-06-26 00:59:15 +0500

    feat: register NoiseMeterProvider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:noise_meter_v2/core/providers/permission_provider.dart';
 import 'package:noise_meter_v2/presentation/main_screen_feature/main_screen.dart';
+import 'package:noise_meter_v2/presentation/main_screen_feature/providers/noise_meter_provider.dart';
 import 'package:provider/provider.dart';
-
 void main() {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => PermissionProvider()..initialCheck())
+        ChangeNotifierProvider(create: (_) => PermissionProvider()..initialCheck()),
+        ChangeNotifierProvider(create: (_) => NoiseMeterProvider()),
       ],
       child: const MyApp(),
     ),
-    
   );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
