c94480584a7058f7d8487c0476282a4810423e0c
Margarita <kkom.margarita@gmail.com>
2026-06-26 22:50:38 +0500

    feat: refactor MainScreen to handle portrait and landscape orientations with LayoutBuilder and AspectRatio components

--- lib/presentation/main_screen_feature/main_screen.dart
+++ lib/presentation/main_screen_feature/main_screen.dart
@@ -1,77 +1,109 @@
 import 'package:flutter/material.dart';
-import 'package:noise_meter_v2/core/providers/permission_provider.dart';
-import 'package:noise_meter_v2/presentation/main_screen_feature/providers/noise_meter_provider.dart';
-import 'package:provider/provider.dart';
 
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-
-  static const List<Widget> _screens = <Widget>[
-      TestScreen1(),
-      TestScreen2()
-  ];
-
-  void _onItemTapped(int index){
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var permissions = context.watch<PermissionProvider>();
-    var startListen = context.watch<NoiseMeterProvider>();
+    var orientation = MediaQuery.of(context).orientation == Orientation.portrait;
 
-    return Scaffold(
-        body: IndexedStack(
-          index: _selectedIndex,
-          children: _screens,
-        ),
-        floatingActionButton: FloatingActionButton(
-          shape: const CircleBorder(),
-          backgroundColor: Colors.white,
-          onPressed: () => startListen.toggleListening(permissions),
-          tooltip: startListen.isListening ? "Start noise meter" : "Stop noise meter",
-          child: startListen.isListening ? Icon(Icons.play_arrow) : Icon(Icons.pause),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home), label: "Home"
+    if(orientation){
+      return Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Container(color: Colors.purple,),
+          ),
+          Expanded(
+              flex: 1,
+              child: Placeholder()
+          ),
+          AspectRatio(
+            aspectRatio: 15/4,
+            child: Placeholder(),
+          ),
+          Expanded(
+              flex: 1,
+              child: Placeholder()
+          ),
+        ],
+      );
+    }else{
+      return Row(
+        children: [
+
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    color: Colors.purple,
+                  ),
+                ),
+              ],
             ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.history_edu), label: "History"
+          ),
+
+          Expanded(
+            child: Column(
+              children: [
+
+                Expanded(
+                  flex: 1,
+                  child: Placeholder(),
+                ),
+
+                LayoutBuilder(
+                  builder: (context, constraints) {
+
+                    if (constraints.maxWidth < 300) {
+                      return const SizedBox();
+                    }
+
+                    return const AspectRatio(
+                      aspectRatio: 15 / 4,
+                      child: Placeholder(),
+                    );
+                  },
+                ),
+
+                Expanded(
+                  flex: 1,
+                  child: Placeholder(),
+                ),
+              ],
             ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.deepPurple,
-          onTap: _onItemTapped,
-        ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    );
+          ),
+        ],
+      );
+    }
   }
 }
 
 
 
-class TestScreen1 extends StatelessWidget {
-  const TestScreen1({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return  Container(color: Colors.red,);
-  }
-}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// leaving for test
 class TestScreen2 extends StatelessWidget {
   const TestScreen2({super.key});
 
