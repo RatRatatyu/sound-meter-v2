fe4e4fb08d2256e1a5ba8fddd6fbe44bd00655a9
Margarita <kkom.margarita@gmail.com>
2026-06-29 22:03:17 +0500

    feat: implement animated collapse and real-time updates in Diagram widget

--- lib/presentation/main_screen_feature/widgets/diagram.dart
+++ lib/presentation/main_screen_feature/widgets/diagram.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+import 'dart:collection';
+
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:noise_meter_v2/presentation/main_screen_feature/providers/noise_meter_provider.dart';
 import 'package:provider/provider.dart';
 
+/// Можно оптимизовать еще
 class Diagram extends StatefulWidget {
   const Diagram({super.key});
 
@@ -11,11 +15,82 @@ class Diagram extends StatefulWidget {
 }
 
 class _DiagramState extends State<Diagram> {
+
+  NoiseMeterProvider? _provider;
+  Timer? _collapseTimer;
+  bool _wasListening = false;
+  ListQueue<double> _localSpots = ListQueue();
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final newProvider = context.read<NoiseMeterProvider>();
+
+    if (_provider != newProvider) {
+      _provider?.removeListener(_providerListener);
+
+      _provider = newProvider;
+      _provider?.addListener(_providerListener);
+    }
+  }
+
+
+  void _providerListener(){
+    final currentProvider = _provider;
+    if (currentProvider == null) return;
+
+    if(currentProvider.isListening){
+      setState(() {
+        _localSpots = ListQueue.of(currentProvider.spots);
+        _wasListening = true;
+      });
+    }else if(_wasListening && !currentProvider.isListening){
+      _wasListening = false;
+      _startCollapseAnimation();
+    }
+  }
+
+  void _startCollapseAnimation(){
+    _collapseTimer?.cancel();
+
+    _collapseTimer = Timer.periodic(
+      const Duration(milliseconds: 50),
+          (timer) {
+        if (_localSpots.isEmpty) {
+          _stopAnimation();
+          return;
+        }
+
+        setState(() {
+          if (_localSpots.isNotEmpty) {
+            _localSpots.removeFirst();
+            _localSpots.addLast(0);
+          }
+        });
+
+        final allZeros = _localSpots.every((db) => db == 0);
+
+        if (allZeros) {
+          _stopAnimation();
+        }
+      },
+    );
+  }
+
+  void _stopAnimation() {
+    _collapseTimer?.cancel();
+    if (!mounted) return;
+    setState(() {
+      _localSpots.clear();
+    });
+    _provider?.cleanSpots();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final data = context.select<NoiseMeterProvider, List<int>>(
-      (provider) => provider.slidingWindow
-    );
+
+    List<double> data= _localSpots.toList();
 
     List<FlSpot> spots = data.isEmpty
         ? [const FlSpot(0, 0)]
@@ -23,15 +98,21 @@ class _DiagramState extends State<Diagram> {
       return FlSpot(index.toDouble(), data[index].toDouble());
     });
 
-
     return LineChart(
       LineChartData(
         lineTouchData: const LineTouchData(enabled: false),
         minY: 0,
         maxY: 120,
-        minX: 20.0,
-        maxX: 0,
+        minX: 0,
+        maxX: 20.0,
         titlesData: const FlTitlesData(
+          leftTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              interval: 20,
+              reservedSize: 40,
+            )
+          ),
           rightTitles: AxisTitles(
             sideTitles: SideTitles(showTitles: false),
           ),
@@ -49,8 +130,8 @@ class _DiagramState extends State<Diagram> {
         lineBarsData: [
           LineChartBarData(
             show: true,
-            spots: spots,
             color: Colors.deepPurple,
+            spots: spots,
             barWidth: 3,
             isCurved: false,
             dotData: const FlDotData(show: false),
@@ -73,5 +154,12 @@ class _DiagramState extends State<Diagram> {
       duration: Duration(milliseconds: 150),
       curve: Curves.linear,
     );
+  }
+
+  @override
+  void dispose() {
+    _collapseTimer?.cancel();
+    _provider?.removeListener(_providerListener);
+    super.dispose();
   }
 }
